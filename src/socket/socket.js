c3b5624a854fb9d1f51f1315ba27d62a7f2026de
Rahul kumar <rahul.kumar@valuecreatives.com>
2026-04-10 18:59:46 +0530

    solved css issue

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -5,10 +5,12 @@ let socket = null;
 export const initSocket = () => {
   if (!socket) {
     const token = localStorage.getItem("token");
+    console.log("Socket token:", token);
+    
 
     socket = io("http://13.232.114.16:8001", {
       auth: { token },
-      transports: ["websocket"],
+      transports: ["polling", "websocket"],
       autoConnect: true
     });
   }
